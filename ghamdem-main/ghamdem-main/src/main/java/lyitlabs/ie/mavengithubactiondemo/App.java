3fe59718e749386808d0f81ef91a1c153094ece4
Ruth.Lennon@atu.ie <ruth.lennon@atu.ie>
2024-10-24 17:50:54 +0100

    initial project

---
+++ ghamdem-main/ghamdem-main/src/main/java/lyitlabs/ie/mavengithubactiondemo/App.java
@@ -0,0 +1,14 @@
+package lyitlabs.ie.mavengithubactiondemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello LYIT!" );
+        
+    }
+}
